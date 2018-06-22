3416bcfde22431e942c4f03889b810967c74fddc
wuyong <1062073083@qq.com>
2018-06-22 22:17:18 +0800

    update the mySet.cpp

---
+++ STL/mySet.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <set>
+
+#include "mySet.h"
+
+void mySetTest()
+{
+
+}
---
+++ STL/mySet.h
@@ -0,0 +1,6 @@
+#ifndef __MYSET_H
+#define __MYSET_H
+
+void mySetTest();
+
+#endif
